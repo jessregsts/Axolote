f3b6b9b004b40204ee5d7ffcee7ec74d956f05e4
Jéssica Regina dos Santos <jessicars241@gmail.com>
2026-01-27 22:36:46 -0300

    Initialize Vue app entry point in main.js

---
+++ front-end/src/main.js
@@ -0,0 +1,10 @@
+// Arquivo de entrada/inicio do app
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './style.css'
+
+createApp(App)
+  .use(router)
+  .mount('#app')
